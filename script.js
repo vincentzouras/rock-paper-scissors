36ab7969385d51234f7554790c6321f5f724e1b0
vincentzouras <vincentzouras@gmail.com>
2024-07-03 11:59:47 -0400

    Created four functions for basic functionality
    These will get the human and computer scores, play a singular match, and play a game of five matches

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("Wad");
+function getComputerChoice() {
+    let num = Math.random();
+    if (num >= 0 && num < .33){
+        return "rock";
+    } else if (num >= .33 && num < .66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+function getHumanChoice(){
+    let choice = prompt("Rock, paper, or scissors?");
+    return choice;
+}
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")){
+        console.log("You win, " + humanChoice + " beats " + computerChoice + "!");
+        humanScore++;
+    } else {
+        console.log("You lose, " + computerChoice + " beats " + humanChoice + ".");
+        computerScore++;
+    }
+}
+function playGame(){
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    console.log(humanScore + " vs " + computerScore);
+    if (humanScore > computerScore){
+        console.log("You win!");
+    } else {
+        console.log("Computer wins.");
+    }
+}
+
+playGame();
